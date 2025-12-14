c02b4b6e66183d3fdebe5c61484baa6463cd41c5
adelefaell <adelefaell@gmail.com>
2025-12-14 02:51:10 +0200

    Refactor project structure by moving asset paths to src directory, updating TypeScript configuration, and removing unused components and files.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -19,14 +19,14 @@ export default function HomeScreen() {
       }
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome Fuck!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
           Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
+          <ThemedText type="defaultSemiBold">src/app/(tabs)/index.tsx</ThemedText>{" "}
           to see changes. Press{" "}
           <ThemedText type="defaultSemiBold">
             {Platform.select({
